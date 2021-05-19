dec6185be586331175cfc86dbcf73f923b32464a
mixelburg <52622705+mixelburg@users.noreply.github.com>
2021-05-19 23:42:16 +0300

    grid bug fix

--- src/About/AboutMe.tsx
+++ src/About/AboutMe.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import AboutMeCard from "./AboutMeCard";
 import IAboutMe from "./IAboutMe";
+import useWindowSize from "../Main/useWindowSize";
 
 interface IAboutMeInfo {
     experience: [IAboutMe],
@@ -16,6 +17,8 @@ const getAboutMe = async (): Promise<IAboutMeInfo | null> => {
 
 
 const AboutMe: React.FC = () => {
+    const windowSize = useWindowSize()
+    const size = windowSize.width > 600 ? "50%" : "90%"
 
     const [about, setAbout] = useState<IAboutMeInfo | null>(null)
 
@@ -25,25 +28,25 @@ const AboutMe: React.FC = () => {
 
     return (
         <div className="d-flex flex-column my-5">
-            <div className="align-self-center" style={{width: "50%"}}>
+            <div className="align-self-center" style={{width: size}}>
                 <div className="text-white h2">
                     About me<span className="text-info">.</span><span className="blink_me">|</span>
                 </div>
 
                 <hr style={{width: "30%", color: "lightblue", height: "4px"}}/>
 
-                <div className="row row-cols-md-2">
+                <div className="row row-cols-1 row-cols-md-2">
                     <div>
                         <div className="text-white h2 mb-4">Experience</div>
                         {
-                            about ? about.experience.map(elem => <AboutMeCard {...elem}/>)
+                            about ? about.experience.map(elem => <AboutMeCard {...elem} key={elem._id}/>)
                                 : <h3>[!] error loading data from server</h3>
                         }
                     </div>
                     <div>
                         <div className="text-white h2 mb-4">Education</div>
                         {
-                            about ? about.education.map(elem => <AboutMeCard {...elem}/>)
+                            about ? about.education.map(elem => <AboutMeCard {...elem} key={elem._id}/>)
                                 : <h3>[!] error loading data from server</h3>
                         }
                     </div>
--- src/Projects/ProjectGrid.tsx
+++ src/Projects/ProjectGrid.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import ProjectCard from "./ProjectCard";
 import IProject from "./IProject"
 import "../static/index.css"
+import useWindowSize from "../Main/useWindowSize";
 
 const getProjects = async (): Promise<IProject[] | null> => {
     const res = await fetch("https://mixelburg.com:5000/projects")
@@ -11,18 +12,17 @@ const getProjects = async (): Promise<IProject[] | null> => {
 }
 
 const ProjectGrid: React.FC = () => {
-
     const [projects, setProjects] = useState<IProject[] | null>(null)
-
+    const windowSize = useWindowSize()
+    const size = windowSize.width > 600 ? "50%" : "90%"
 
     useEffect(() => {
         getProjects().then(data => setProjects(data))
     }, [])
 
-
     return (
         <div className="d-flex flex-column mt-3">
-            <div className="align-self-center" style={{width: "50%"}}>
+            <div className="align-self-center" style={{width: size}}>
 
                 <div className="text-white h2">
                     Projects<span className="text-info">.</span><span className="blink_me">|</span>
@@ -30,7 +30,7 @@ const ProjectGrid: React.FC = () => {
 
                 <hr style={{width: "30%", color: "lightblue", height: "4px"}}/>
 
-                <div className="row row-cols-md-2 g-4">
+                <div className="row row-cols-1 row-cols-sm-2 g-4">
                     {
                         projects ? projects.map(project => <ProjectCard {...project} key={project._id}/>)
                             : <h3>[!] error loading data from server</h3>
@@ -43,6 +43,7 @@ const ProjectGrid: React.FC = () => {
                         projects ? projects.map(project => <ProjectCard {...project} key={project._id}/>)
                             : <h3>[!] error loading data from server</h3>
                     }
+
                 </div>
             </div>
         </div>
