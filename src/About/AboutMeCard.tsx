92f312f2283febf4df2387956cbfdfb790068ad6
mixelburg <52622705+mixelburg@users.noreply.github.com>
2021-05-19 23:03:35 +0300

    spacing fix

--- src/About/AboutMeCard.tsx
+++ src/About/AboutMeCard.tsx
@@ -3,7 +3,7 @@ import IAboutMe from "./IAboutMe";
 
 const AboutMeCard: React.FC<IAboutMe> = (props) => {
     return (
-        <div className="ms-3" style={{width: "80%"}}>
+        <div className="ms-3 mb-5" style={{width: "80%"}}>
             <div className="text-white h3 mb-0">{props.header}</div>
             <div className="text-white h5" style={{opacity: "50%"}}>
                 {props.sub_header}
