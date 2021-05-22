f2aeaed8a486beb08d0233285bf844dc69aaa255
mixelburg <52622705+mixelburg@users.noreply.github.com>
2021-05-22 23:26:22 +0300

    styling changed

--- src/Main/MainInfo.tsx
+++ src/Main/MainInfo.tsx
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from "react";
 import Avatar from "../static/ava.png"
 import useWindowSize from "../util/useWindowSize";
 import ProgressBarSlider from "./ProgressBarSlider";
-import {CSSTransition, TransitionGroup} from "react-transition-group";
 import "./MainInfo.css"
 import "../static/index.css"
 
@@ -13,11 +12,15 @@ const MainInfo: React.FC = () => {
     const size = windowSize.width > 600 ? "70%" : "90%"
 
     const [curr, setCurr] = useState(0)
+    const [slider, setSlider] = useState(1)
 
     useEffect(() => {
         setTimeout(() => {
-            setCurr(prevState => ((curr + 1) % 2))
-        }, 5000)
+            setCurr(prevState => ((curr + 1) % 10))
+        }, 1000)
+        if (curr % 5 === 0) {
+            setSlider(prevState => prevState * -1)
+        }
     }, [curr])
 
 
@@ -26,29 +29,26 @@ const MainInfo: React.FC = () => {
             <div className="d-flex flex-row  align-self-center mx-2" style={{width: size}}>
                 <img style={{width: "30%", height: "30%"}} className="rounded-circle position-relative p-2" src={Avatar}  alt="..."/>
 
+                // TODO: split into separate cards
                 <div className="d-flex flex-column">
                     <div className="text-white main-text">
+                        {
+                            (curr >=0 && curr<=4) &&
+                            <div className="my-text-main">
+                                <div className="h2 m-0" style={{fontSize: secSize}}>My name is</div>
+                                <div className="h1" style={{fontSize: mainSize, lineHeight: "60%"}}>Ivan Krokha</div>
+                            </div>
+                        }
+                        {
+                            (curr >=5 && curr<=9) &&
+                            <div className="my-text-main">
+                                <div className="h2 m-0" style={{fontSize: secSize}}>I am a Full-stack</div>
+                                <div className="h1" style={{fontSize: mainSize, lineHeight: "60%"}}>Web Developer</div>
+                            </div>
+                        }
 
-                        {(() => {
-                            switch (curr) {
-                                case 0:
-                                    return (
-                                        <div>
-                                            <div className="h2 m-0" style={{fontSize: secSize}}>My name is</div>
-                                            <div className="h1" style={{fontSize: mainSize, lineHeight: "60%"}}>Ivan Krokha</div>
-                                        </div>
-                                    )
-                                case 1:
-                                    return (
-                                        <div>
-                                            <div className="h2 m-0" style={{fontSize: secSize}}>I am a Full-stack</div>
-                                            <div className="h1" style={{fontSize: mainSize, lineHeight: "60%"}}>Web Developer</div>
-                                        </div>
-                                    )
-                            }
-                        })()}
                     </div>
-                    <ProgressBarSlider key={curr}/>
+                    <ProgressBarSlider key={slider}/>
                 </div>
 
             </div>
