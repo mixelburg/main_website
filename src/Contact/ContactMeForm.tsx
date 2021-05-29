322a07e192127ab1ce537c742c260cb222c7c7a8
mixelburg <52622705+mixelburg@users.noreply.github.com>
2021-05-29 12:45:20 +0300

    contact me form styling updated

--- src/Contact/ContactMeForm.tsx
+++ src/Contact/ContactMeForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./ContactMe.css"
 
 interface Props {
     fields: {
@@ -16,48 +17,57 @@ interface Props {
 
 const ContactMeForm: React.FC<Props> = (props) => {
 
-    const nameClass = `form-control ${props.errors.name === "" ? "is-valid" : "is-invalid"}`
-    const emailClass =`form-control ${props.errors.email === "" ? "is-valid" : "is-invalid"}`
-    const msgClass = `form-control ${props.errors.message === "" ? "is-valid" : "is-invalid"}`
-
     return (
-        <form>
-            <div className="row gx-5 gy-2">
-                <div className="col-12 col-md-6" style={{height: "6rem"}}>
-                    <label className="form-label text-info">name</label>
-                    <input name="name" type="text"
-                           className={nameClass}
-                           value={props.fields.name}
-                           onChange={props.handleChange}
-                    />
-                    <div className="form-text text-danger">{props.errors.name}</div>
+        <div>
+            <form>
+                <div className="row gx-5 gy-2">
+                    <div className="col-12 col-md-6" style={{height: "6rem"}}>
+                        <div className="field text-info">
+                            <input className="bg-transparent text-white" type="text"
+                                   name="name" id="fullname"
+                                   placeholder="Iron Man"
+                                   value={props.fields.name}
+                                   onChange={props.handleChange}
+                            />
+                            <label className="mb-1" htmlFor="fullname">Name</label>
+                        </div>
+                        <div className="form-text text-danger">{props.errors.name}</div>
+                    </div>
+                    <div className="col-12 col-md-6" style={{height: "6rem"}}>
+                        <div className="field text-info">
+                            <input className="bg-transparent text-white" type="email"
+                                   name="email" id="email"
+                                   placeholder="irom.man@example.com"
+                                   value={props.fields.email}
+                                   onChange={props.handleChange}
+                            />
+                            <label className="mb-1" htmlFor="email">Email</label>
+                        </div>
+                        <div className="form-text text-danger">{props.errors.email}</div>
+                    </div>
+                    <div className="col-12" style={{height: "8rem"}}>
+                        <div className="field text-info">
+                            <textarea name="message" id="message"
+                                      style={{resize: "none"}}
+                                      className="bg-transparent text-white"
+                                      rows={3}
+                                      value={props.fields.message}
+                                      onChange={props.handleChange}
+                            />
+                            <label className="mb-1" htmlFor="message">Message</label>
+                        </div>
+                        <div className="form-text text-danger">{props.errors.message}</div>
+                    </div>
                 </div>
-                <div className="col-12 col-md-6" style={{height: "6rem"}}>
-                    <label className="form-label text-info">email</label>
-                    <input name="email" type="email"
-                           className={emailClass}
-                           value={props.fields.email}
-                           onChange={props.handleChange}
-                    />
-                    <div className="form-text text-danger">{props.errors.email}</div>
+                <div className="form-text h6 text-secondary" style={{fontSize: "70%"}}>
+                    This site is protected by reCAPTCHA and the Google
+                    <a href="https://policies.google.com/privacy">Privacy Policy</a> and
+                    <a href="https://policies.google.com/terms">Terms of Service</a> apply.
                 </div>
-                <div className="col-12">
-                    <label className="form-label text-info">message</label>
-                    <textarea name="message"
-                              className={msgClass}
-                              rows={4} cols={50}
-                              value={props.fields.message}
-                              onChange={props.handleChange}
-                    />
-                    <div className="form-text text-danger">{props.errors.message}</div>
-                </div>
-            </div>
-            <div className="form-text h6 text-secondary" style={{fontSize: "70%"}}>
-                This site is protected by reCAPTCHA and the Google
-                <a href="https://policies.google.com/privacy">Privacy Policy</a> and
-                <a href="https://policies.google.com/terms">Terms of Service</a> apply.
-            </div>
-        </form>
+            </form>
+
+        </div>
+
     );
 };
 
